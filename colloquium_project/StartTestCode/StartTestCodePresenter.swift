bcfa7003cfed6431f369f2533723eaffb75b09e8
vovaggri <vladimirgri0405@gmail.com>
2025-09-24 01:30:15 +0300

    Added test code ui

---
+++ colloquium_project/StartTestCode/StartTestCodePresenter.swift
@@ -0,0 +1,16 @@
+//
+//  StartTestCodePresenter.swift
+//  colloquium_project
+//
+//  Created by Vladimir Grigoryev on 23.09.2025.
+//
+
+import Foundation
+
+protocol StartTestCodeInteractorOutput {
+    
+}
+
+final class StartTestCodePresenter: StartTestCodeInteractorOutput {
+    weak var vc: StartTestCodeVC?
+}
