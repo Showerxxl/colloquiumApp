185bc63d0ea02d9fd705483c6b0a539dc3a98d11
lzkgmr <lzkgmr@gmail.com>
2025-09-24 22:56:19 +0300

    trying to do smth

--- colloquium_project/AssistantStart/AssistantStartViewController.swift
+++ colloquium_project/AssistantStart/AssistantStartViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class AssistantStartViewController: UIViewController {
     private let interactor: AssistantStartInteractionLogic
     
+    private let accountButton = UIButton(type: .system)
+    
     init(interactor: AssistantStartInteractionLogic) {
         self.interactor = interactor
         super.init(nibName: nil, bundle: nil)
@@ -17,5 +19,14 @@ final class AssistantStartViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        
     }
 }
